69d0a107441217bdd44398eea70a3167c8a8c4d3
joshuotaXD <157252362+joshuotaXD@users.noreply.github.com>
2026-08-10 08:44:02 +0000

    fix: corregir llamada a la variable de entorno en resend

--- app/api/reset-password/route.js
+++ app/api/reset-password/route.js
@@ -2,7 +2,7 @@ import { NextResponse } from 'next/server';
 import { Resend } from 'resend';
 
 // Inicializa Resend con tu API Key (debes obtenerla en resend.com)
-const resend = new Resend(re_UbwWNGK3_QEV87VRWywCsHmDbKeuAeSB8);
+const resend = new Resend(process.env.RESEND_API_KEY);
 
 export async function POST(request) {
   try {
