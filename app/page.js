5d4b7d733d55b72162847713621ef3828ccc7ece
joshuotaXD <157252362+joshuotaXD@users.noreply.github.com>
2026-08-11 23:40:37 +0000

    imagen

--- app/page.js
+++ app/page.js
@@ -4,7 +4,7 @@ export const dynamic = 'force-dynamic';
 import { useState, useEffect } from 'react';
 import Image from 'next/image';
 import { supabase } from '@/lib/supabase';
-import Gallery from './components/Gallery';
+
 
 export default function Home() {
   const [activeSection, setActiveSection] = useState('inicio');
