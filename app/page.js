c38c6eee62df58dc92b4e55cb6670d0bc0c2740c
joshuotaXD <157252362+joshuotaXD@users.noreply.github.com>
2026-08-12 21:02:25 +0000

    forgot password

--- app/page.js
+++ app/page.js
@@ -35,7 +35,7 @@ const handlePasswordReset = async (e) => {
     setSuccessMessage('');
 
    try {
-      const response = await fetch('/api/recovery', { // Asegúrate de que apunte a tu ruta de API
+      const response = await fetch('/api/forgot-password', { // Asegúrate de que apunte a tu ruta de API
         method: 'POST',
         headers: { 'Content-Type': 'application/json' },
         body: JSON.stringify({ email: resetEmail }),
